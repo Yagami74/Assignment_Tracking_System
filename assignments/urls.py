cb0dc47a5b1d8f151896dbe1dfcf3ab0a5cb3392
Yagami <samirdangal4@gmail.com>
2026-08-12 10:26:12 +0545

    Post and ger operation with swagger

---
+++ assignments/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import AssignmentViewSet
+
+urlpatterns =[
+    #GET /api/assignments/          -> list all 
+    #POST /api/assignments/          -> create one
+    path("",AssignmentViewSet.as_view({"get": "list", "post": "create"})),
+]
+
